2e9b6e500b09fe1415cd57d046d736b665ec2188
why-lyl <liuyulonggz@163.com>
2020-03-31 00:36:11 +0800

    the first test Github

---
+++ springBootDemo/src/main/java/com/lyl/springBootDemo/modules/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.lyl.springBootDemo.modules.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	@RequestMapping("/test/demoInfo")
+	@ResponseBody
+	public String demoDesc() {
+		return "这是一次成功的操作";
+	}
+
+}
